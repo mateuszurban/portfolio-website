633f70d16ae22b93159b2c99e80a5f527552d26f
mateuszurban <65718811+mateuszurban@users.noreply.github.com>
2024-05-07 15:20:54 +0200

    Add missing aria labels

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,14 +2,24 @@ export default function Footer({ scrollOnClick }) {
   return (
     <footer>
       <ul>
-        <a href="https://github.com/mateuszurban">
+        <a
+          href="https://github.com/mateuszurban"
+          aria-label="Visit my Github profile"
+        >
           <li className="footerIconGithub"></li>{" "}
         </a>
 
-        <a href="https://linkedin.com/in/mateusz-urban-04b822298">
+        <a
+          href="https://linkedin.com/in/mateusz-urban-04b822298"
+          aria-label="Visit my LinkedIn profile"
+        >
           <li className="footerIconLinkedin"></li>{" "}
         </a>
-        <li className="footerIconTop" onClick={() => scrollOnClick("content")}>
+        <li
+          className="footerIconTop"
+          aria-label="Back to top"
+          onClick={() => scrollOnClick("content")}
+        >
           {" "}
         </li>
       </ul>
